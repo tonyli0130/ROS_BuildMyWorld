3a251972cbbd6cf62737a0eb5ac53c4477ed9f75
Zilin Li <tonyli19970130@gmail.com>
2020-07-19 12:00:34 -0500

    First commit

---
+++ script/hello.cpp
@@ -0,0 +1,17 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+  class WorldPluginBuildMyWorld : public WorldPlugin
+  {
+    public: WorldPluginBuildMyWorld() : WorldPlugin()
+            {
+              printf("Welcome to Zilin's World!\n");
+            }
+
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+            {
+            }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginBuildMyWorld)
+}
